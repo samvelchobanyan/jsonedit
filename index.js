a429b787b8d44a490ffc5835df829b0117ad1236
samvel <samvel.chobanyan@gmail.com>
2021-10-21 20:46:58 +0400

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('./index.html')
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', function () {
+    console.log('exit exanq')
+    if (process.platform !== 'darwin') app.quit()
+})
